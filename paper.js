2b3ce66ad30b7f01d02406285b522f444538c4c9
revi002 <75730764+revi002@users.noreply.github.com>
2021-02-01 16:58:12 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,34 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+restitution:0.3,
+friction:0.5,
+density:1.2			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x,this.y, this.r/2,  options);
+		this.image=loadImage("paper.png")
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			imageMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			image(this.image,0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
